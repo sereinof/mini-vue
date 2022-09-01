1cca42e208e21e800e154cbafcd1e96adafa171e
sereinof <1099209547@qq.com>
2022-09-01 23:18:12 +0800

    feat:模版编译开始

---
+++ packages/compiler-core/src/ast.ts
@@ -0,0 +1,19 @@
+export const enum NodeTypes {
+ROOT,//跟节点
+ELEMENT,//元素
+TEXT,//文本
+COMMENT,//注释
+SIMPLE_EXPRESSION,//简单表达
+INTERPOLATION,//模版表达式
+ATTRIBUTE,
+DIRECTIVE,
+//containers
+COMPOUND_EXPRESSION,//复合表达式
+IF,
+If_BRANCH,
+FOR,
+TEXT_CALL,//文本调用
+//codegen
+VNODE_CALL,//元素调用
+JS_CALL_EXPRESS,//js调用表达式
+}
---
+++ packages/compiler-core/src/index.ts
@@ -0,0 +1,13 @@
+function parse(template){
+//创建一个解析的上下文，来进行处理
+}
+
+export function compile(template){
+  const ast =  parse(template);//这里需要将html语法转换成javascript语法
+ //对ast语法书尽心一些预先处理
+
+ //Transform(ast);
+console.log('fgvdfbdfbdf')
+ //return generate(ast);
+
+}
